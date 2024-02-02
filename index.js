5bb6e289330ec155d52bfbcbb3f11869083da103
Erojas88 <erojas@dt-ip.com>
2024-02-02 15:45:27 +0100

    index.js version 1.1

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.use('/api/auth', require('./routes/auth') );
 // TODO: CRUD: Eventos 
 app.use('/api/events', require('./routes/events') );
 
+app.get('*', (req, res ) => {
+    res.sendFile(__dirname + '/Public/index.html');
+})
+
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
